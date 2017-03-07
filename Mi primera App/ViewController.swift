db11963d199aafc92d1451ea823326b8829f4d57
taysir.al <taysir.al@live.u-tad.com>
2017-03-07 09:43:15 +0100

    Initial Commit

---
+++ Mi primera App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mi primera App
+//
+//  Created by TAYSIR AL-SHAREEF PINERO on 7/3/17.
+//  Copyright © 2017 TAYSIR AL-SHAREEF PINERO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
