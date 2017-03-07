ff99b97105922dbd601f96f591737557127a6d35
taysir.al <taysir.al@live.u-tad.com>
2017-03-07 12:49:14 +0100

    Root Commit

---
+++ Mi primera App/VCPrincipal.swift
@@ -0,0 +1,35 @@
+//
+//  VCPrincipal.swift
+//  Mi primera App
+//
+//  Created by TAYSIR AL-SHAREEF PINERO on 7/3/17.
+//  Copyright © 2017 TAYSIR AL-SHAREEF PINERO. All rights reserved.
+//
+
+import UIKit
+
+class VCPrincipal: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
